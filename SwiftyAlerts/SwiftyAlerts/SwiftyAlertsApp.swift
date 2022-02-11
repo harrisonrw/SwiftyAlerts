7ae7605a12f8d3dfec56fa3da152d196b2386013
Robert Harrison <harrisonrw@gmail.com>
2022-02-11 13:11:05 -0800

    Initial Xcode Project

---
+++ SwiftyAlerts/SwiftyAlerts/SwiftyAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyAlertsApp.swift
+//  SwiftyAlerts
+//
+//  Created by Robert Harrison on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
